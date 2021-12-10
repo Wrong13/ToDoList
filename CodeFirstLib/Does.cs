86be37fdfae3de6293d85a41775af600f51e1f1b
Wrong13 <ilak71363@gmail.com>
2021-12-10 17:29:28 +0600

    Аннотации

--- CodeFirstLib/Does.cs
+++ CodeFirstLib/Does.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,16 @@ namespace CodeFirstLib
 {
     public class Does
     {
+        [Required]
+        [Key]
         public int ID { get; set; }
+        [Required]
+        [MinLength(5)]
+        [MaxLength(150)]
         public string Doe { get; set; }
         public string Description { get; set; }
-        public int ListID { get; set; }
+        [Key]
+        public int ListsID { get; set; }
         public string Date { get; set; }
     }
 }
--- CodeFirstLib/Lists.cs
+++ CodeFirstLib/Lists.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,14 @@ namespace CodeFirstLib
 {
     public class Lists
     {
+        [Required]
+        [Key]
         public int ID { get; set; }
+        [MinLength(5)]
+        [MaxLength(50)]
+        [Required]
         public string NameLists { get; set; }
+        
         public string Description { get; set; }
 
         public string Img { get; set; }
