3fe0ca8060e3d17d2025f8824d1ebbb4429563e1
Wrong <ilak71363@gmail.com>
2022-01-11 13:06:18 +0600

    Edit p1

---
+++ ToDoList/Windows/DoWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ToDoList.CodeFirst;
+
+namespace ToDoList.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для DoWindow.xaml
+    /// </summary>
+    public partial class DoWindow : Window
+    {
+        public Lists Lists { get;private set; }
+        public DoWindow(Lists lists)
+        {
+            InitializeComponent();
+            Lists = lists;
+            this.DataContext = Lists;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+    }
+}
